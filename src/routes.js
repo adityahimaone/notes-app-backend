3780262073b096d2ec93b43b36d2acca0b1aa20f
adityahimaone <adityahimaone@gmail.com>
2021-05-03 14:41:54 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+/* eslint linebreak-style: ["error", "windows"]*/
+const {
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNotebyIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNotebyIdHandler,
+  },
+];
+
+
+module.exports = routes;
